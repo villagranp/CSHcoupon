2f0fb4d567660a6d85dd1d29c19c67fb1249994f
Pablo Villagran Torres <pablo.villagran@liztex.com>
2018-05-16 17:27:27 -0600

    commit al 160518

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
 
 export default class App extends Component {
   render() {
     const image = 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Logo-Chivas-GP.png'
     const name  = 'Chivas de Guadalajara'
+    const likes = 100
     return (
       <View style={styles.container}>
         <View style={styles.box}>
             <Image style={styles.image} source={{uri: image }} />
             <View style={styles.textInBox}>
-              <Text style={styles.textBox}> {name} </Text>
+              <View style={styles.textApart}>
+                <Text style={styles.textBox}> {name} </Text>
+              </View>
+              <View style={styles.iconApart}>
+                <Foundation name='like' size={35} color='blue' />
+                <Text > {likes} </Text>
+              </View>
             </View>
         </View>
       </View>
@@ -34,12 +42,18 @@ const styles = StyleSheet.create({
   },
   textInBox:{
     flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
   },
   textBox:{
     fontSize: 25,
     marginTop: 10,
-    color: 'steelblue'
-  }
+    color: 'steelblue',
+  },
+  textApart:{
+    backgroundColor: 'red',
+  },
+  iconApart:{
+    flex: 1,
+    backgroundColor: 'green',
+  },
 });
